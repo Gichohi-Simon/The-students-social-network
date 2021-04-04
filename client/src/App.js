4250b62de34e04f380fde61d3a8315db74df07aa
Gichohi <njogugichohi56@gmail.com>
2021-04-04 20:35:54 +0300

    off to learn some more material ui, i will be back soon

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import './App.css';
+import {Container,AppBar,Toolbar,Typography,Button} from '@material-ui/core';
+import useStyles from './style'
 
-function App() {
-  return (
-    <div className="App">
-     hello
-    </div>
-  );
+const App = () => {
+
+const classes = useStyles();
+
+  return (  
+    <Container maxwidth="lg">
+      <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+              <Typography variant="h5" className={classes.title}>
+                Student Social Network.
+              </Typography>
+              <Typography variant="h6" className={classes.userName} align="right">
+                Gichohi_Simon
+              </Typography>
+              <Button variant="contained" style={{backgroundColor:"#00ACEE", color:'#ffffff'}}>
+                  logout
+              </Button>
+          </Toolbar>
+      </AppBar>
+    </Container>
+   );
 }
-
+ 
 export default App;
