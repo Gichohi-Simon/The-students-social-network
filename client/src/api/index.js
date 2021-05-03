c22964acad733ccd97b5e9250ee7d52cb2852e82
Gichohi <njogugichohi56@gmail.com>
2021-05-03 16:13:34 +0300

    ran into a tiny error

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,12 +2,11 @@ import axios from 'axios';
 
 const API = axios.create({baseURL:'http://localhost:5000/posts'})
 
-
 export const fetchPosts = () => API.get('/posts');
 export const createPost = (newPost) => API.post('/posts',newPost);
 export const updatePost = (id,updatePost) => API.patch(`/posts/${id}`,updatePost);
 export const likePost = (id) => API.patch(`/posts/${id}/likePost`);
 export const deletePost = (id) => API.delete(`/posts/${id}`);
 
-export const signIn = (formData) => API.post('/user/signin',formData);
-export const signUp = (formData) => API.post('/user/signup',formData);
+export const signIn = (formData) => API.post('/users/signin',formData);
+export const signUp = (formData) => API.post('/users/signup',formData);
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const users = require('../controllers/user');
+const users = require('../controllers/users');
 
 router.post('/signin', users.signin);
 router.post('/signup', users.signup);
